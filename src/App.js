70395b931ef1864050bd8a92399914d24fd02e2b
Francis Rodrigues <francis.rod01@outlook.com>
2018-06-21 00:18:54 -0300

    Implemented HomeScreen to App component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,16 @@ import { SafeAreaView } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
 
+const HomeScreen = ({ navigation }) => (
+  <SafeAreaView>
+    <Button
+      title="Go to details"
+      onPress={() => navigation.navigate('Details')}
+    />
+  </SafeAreaView>
+);
+
+
 export default class App extends React.Component {
   render() {
     return (
